f34d93d26a5c19a83df43ab2edb85d07ba61b85f
Roland Dordevic <rdorde@gmail.com>
2024-01-01 20:17:53 +0100

    POST form to BE

---
+++ src/routes/boat/+page.server.ts
@@ -0,0 +1,19 @@
+import { Actions } from './../../../.svelte-kit/types/src/routes/boat/$types.d';
+import type { PageServerLoad, Actions } from './$types';
+
+export const load = (async ({ cookies }) => {
+  const boatName = cookies.get('boatName', { path: '/boat' });
+
+  return {
+    boatName
+  };
+}) satisfies PageServerLoad;
+
+export const actions: Actions = {
+  default: async ({ request, cookies }) => {
+    const formData = await request.formData();
+    const boatName = formData.get('boatName') as string;
+
+    cookies.set('boatName', boatName, { path: '/boat' });
+  }
+};
